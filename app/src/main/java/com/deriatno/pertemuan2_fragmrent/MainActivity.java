6e4630a2ca8cebd7af033fe41f0caf94283029f4
Deriatno02 <deriatno05@gmail.com>
2022-10-18 16:32:39 +0700

    initial commit

---
+++ app/src/main/java/com/deriatno/pertemuan2_fragmrent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.deriatno.pertemuan2_fragmrent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
